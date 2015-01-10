006d75e2f801fa87427731fcafde6f9641e88a84
Warren Mira <warrenmira@gmail.com>
2015-01-10 23:53:48 +0800

    moving to webpack

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+
+
+var plugins = [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
+    })
+];
+
+if (process.env.COMPRESS) {
+
+    plugins.push(
+
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    );
+}
+
+module.exports = {
+
+    output: {
+        library: 'RDT',
+        libraryTarget: 'umd2'
+    },
+
+    externals: {
+        react: {
+            root: 'React',
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react'
+        }
+    },
+
+
+    plugins: plugins,
+
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'jsx-loader?harmony' }
+        ]
+    }
+
+};
